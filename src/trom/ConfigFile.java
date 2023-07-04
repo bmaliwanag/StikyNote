6baa42f24c1f8cc743d521ea599014cfd325fbf5
bmaliwanag <maliwanagbrian@gmail.com>
2023-07-04 14:44:49 -0500

    Added ConfigFile v1
    Added a config file for constants related to both restaurant rates and program

---
+++ src/trom/ConfigFile.java
@@ -0,0 +1,15 @@
+package trom;
+
+/*
+ * Class: Configuration File
+ * This class contains constants for the program
+ * @public static final float taxRate = 8.25%
+ * @public static final int maxTicketAmount = 10
+ * @public static final float employeeDiscount = 15%
+ */
+
+public class ConfigFile {
+	public static final float taxRate = 8.25f;
+	public static final int maxTicketAmount = 10;
+	public static final float employeeDiscount = 15f;
+}
