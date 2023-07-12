859db826a49938b3278b75d5b067cfa575313481
bmaliwanag <maliwanagbrian@gmail.com>
2023-07-11 22:35:47 -0500

    Added a ticket prefab for testing

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -19,15 +19,17 @@ import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLayeredPane;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class MainGUI extends JFrame {
 
 	private JPanel contentPane;
+	private String[] status = {"Unpaid","Pending","Complete"};
 	private JTextField nameDisplay;
 	private JTextField phoneDisplay;
 	private JTextField priceDisplay;
-	private String[] status = {"Unpaid","Pending","Complete"};
-	private JPanel ticketPanel;
 
 	/**
 	 * Launch the application.
@@ -63,135 +65,158 @@ public class MainGUI extends JFrame {
 		toolBar.setBounds(0, 0, 1256, 30);
 		contentPane.add(toolBar);
 		
-		JButton debugButton = new JButton("DebugTest");
-		toolBar.add(debugButton);
-		
-		ticketPanel = new JPanel();
-		ticketPanel.setVisible(false);
-		ticketPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		ticketPanel.setBackground(new Color(255, 255, 128));
+		JLayeredPane ticketPanel = new JLayeredPane();
 		ticketPanel.setBounds(10, 40, 222, 258);
 		contentPane.add(ticketPanel);
-		ticketPanel.setLayout(null);
 		
-		JLabel nameLabel = new JLabel("Name:");
+		JPanel ticket = new JPanel();
+		ticket.setVisible(false);
+		ticketPanel.setLayer(ticket, 1);
+		ticket.setBackground(new Color(255, 255, 191));
+		ticket.setBounds(0, 0, 222, 258);
+		ticketPanel.add(ticket);
+		ticket.setLayout(null);
+		
+		JLabel nameLabel = new JLabel("Name: ");
 		nameLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		nameLabel.setBounds(10, 10, 85, 27);
-		ticketPanel.add(nameLabel);
+		nameLabel.setIconTextGap(2);
+		nameLabel.setBounds(10, 10, 81, 19);
+		ticket.add(nameLabel);
 		
 		nameDisplay = new JTextField();
-		nameDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		nameDisplay.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		nameDisplay.setEditable(false);
-		nameDisplay.setBackground(new Color(255, 255, 255));
-		nameDisplay.setBounds(10, 33, 169, 28);
-		ticketPanel.add(nameDisplay);
+		nameDisplay.setBounds(10, 30, 168, 19);
+		ticket.add(nameDisplay);
 		nameDisplay.setColumns(10);
 		
 		JLabel phoneLabel = new JLabel("Phone #:");
+		phoneLabel.setIconTextGap(2);
 		phoneLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		phoneLabel.setBounds(10, 71, 85, 27);
-		ticketPanel.add(phoneLabel);
+		phoneLabel.setBounds(10, 50, 81, 19);
+		ticket.add(phoneLabel);
 		
 		phoneDisplay = new JTextField();
-		phoneDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		phoneDisplay.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		phoneDisplay.setEditable(false);
 		phoneDisplay.setColumns(10);
-		phoneDisplay.setBackground(Color.WHITE);
-		phoneDisplay.setBounds(10, 94, 169, 28);
-		ticketPanel.add(phoneDisplay);
+		phoneDisplay.setBounds(10, 70, 168, 19);
+		ticket.add(phoneDisplay);
 		
-		JLabel priceLabel = new JLabel("Grand Total:");
+		JLabel priceLabel = new JLabel("Amount Due:");
+		priceLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+		priceLabel.setIconTextGap(2);
 		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		priceLabel.setBounds(127, 124, 85, 27);
-		ticketPanel.add(priceLabel);
+		priceLabel.setBounds(108, 90, 104, 19);
+		ticket.add(priceLabel);
 		
 		priceDisplay = new JTextField();
-		priceDisplay.setForeground(new Color(0, 255, 0));
-		priceDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		priceDisplay.setHorizontalAlignment(SwingConstants.RIGHT);
-		priceDisplay.setEditable(false);
 		priceDisplay.setBackground(new Color(0, 0, 0));
-		priceDisplay.setBounds(109, 147, 103, 28);
-		ticketPanel.add(priceDisplay);
+		priceDisplay.setForeground(new Color(0, 255, 0));
+		priceDisplay.setHorizontalAlignment(SwingConstants.TRAILING);
+		priceDisplay.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		priceDisplay.setEditable(false);
 		priceDisplay.setColumns(10);
-		
-		JLabel statusLabel = new JLabel("Order Status:");
-		statusLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		statusLabel.setBounds(10, 124, 107, 27);
-		ticketPanel.add(statusLabel);
+		priceDisplay.setBounds(108, 111, 104, 34);
+		ticket.add(priceDisplay);
 		
 		JButton cancelButton = new JButton("Cancel");
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int response = JOptionPane.showConfirmDialog(contentPane,"Are you sure you want to cancel this ticket?","Confirm Deletion",JOptionPane.YES_NO_OPTION);
-				if(response == 0) {
-					nameDisplay.setText(""); phoneDisplay.setText("");ticketPanel.setVisible(false);
-				}
-			}
-		});
-		cancelButton.setBackground(new Color(255, 128, 128));
-		cancelButton.setBounds(10, 179, 202, 21);
-		ticketPanel.add(cancelButton);
-		
-		JButton editButton = new JButton("Edit");
-		editButton.setBackground(new Color(255, 255, 0));
-		editButton.setBounds(10, 199, 101, 21);
-		ticketPanel.add(editButton);
-		
-		JButton viewButton = new JButton("View");
-		viewButton.setEnabled(false);
-		viewButton.setBackground(new Color(115, 185, 255));
-		viewButton.setBounds(111, 199, 101, 21);
-		ticketPanel.add(viewButton);
-		
-		JButton finishButton = new JButton("Finish");
-		finishButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ticketPanel.setVisible(false);
-			}
-		});
-		finishButton.setEnabled(false);
-		finishButton.setBackground(new Color(0, 255, 0));
-		finishButton.setBounds(10, 227, 202, 21);
-		ticketPanel.add(finishButton);
-		
-
-		JComboBox<String> statusBox = new JComboBox<String>(status);
-		statusBox.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == statusBox) {
-					switch(statusBox.getSelectedIndex()) {
-					case 0:
-						cancelButton.setEnabled(true);
-						editButton.setEnabled(true);
-						viewButton.setEnabled(false);
-						finishButton.setEnabled(false);
-						break;
-					case 1:
-						cancelButton.setEnabled(false);
-						editButton.setEnabled(false);
-						viewButton.setEnabled(true);
-						finishButton.setEnabled(false);
-						break;
-					case 2:
-						cancelButton.setEnabled(false);
-						editButton.setEnabled(false);
-						viewButton.setEnabled(true);
-						finishButton.setEnabled(true);
-						break;
+				if(e.getSource() == cancelButton) {
+					if(JOptionPane.showConfirmDialog(contentPane,"Are you sure you want to cancel this order?","Order Cancellation",JOptionPane.YES_NO_OPTION) == 0) {
+						nameDisplay.setText("");phoneDisplay.setText("");priceDisplay.setText("");
+						ticket.setVisible(false);
+						ticketPanel.getComponentsInLayer(0)[0].setVisible(true);
 					}
 				}
 			}
 		});
-		statusBox.setBounds(10, 147, 96, 28);
-		ticketPanel.add(statusBox);
+		cancelButton.setBackground(new Color(255, 128, 128));
+		cancelButton.setForeground(new Color(0, 0, 0));
+		cancelButton.setBounds(10, 155, 202, 31);
+		ticket.add(cancelButton);
 		
-		debugButton.addActionListener(new ActionListener() {
+		JButton editButton = new JButton("Edit");
+		editButton.setBackground(new Color(255, 255, 0));
+		editButton.setBounds(10, 186, 101, 31);
+		ticket.add(editButton);
+		
+		JButton viewButton = new JButton("View");
+		viewButton.setEnabled(false);
+		viewButton.setBackground(new Color(102, 179, 255));
+		viewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == debugButton) {
-					ticketPanel.setVisible(true);
+			}
+		});
+		viewButton.setBounds(111, 186, 101, 31);
+		ticket.add(viewButton);
+		
+		JButton completeButton = new JButton("Cash In");
+		completeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == completeButton) {
+					if(JOptionPane.showConfirmDialog(contentPane,"Has the customer recieved their food?","Order Completion",JOptionPane.YES_NO_OPTION) == 0) {
+						nameDisplay.setText("");phoneDisplay.setText("");priceDisplay.setText("");
+						ticket.setVisible(false);
+						ticketPanel.getComponentsInLayer(0)[0].setVisible(true);
+					}
 				}
 			}
 		});
+		completeButton.setEnabled(false);
+		completeButton.setBackground(new Color(0, 255, 128));
+		completeButton.setBounds(10, 217, 202, 31);
+		ticket.add(completeButton);
+		
+		JComboBox<String> statusBox = new JComboBox<String>(status);
+		statusBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getSource() == statusBox) {
+					switch(statusBox.getSelectedIndex()) {
+						case 0:
+							cancelButton.setEnabled(true);
+							editButton.setEnabled(true);
+							viewButton.setEnabled(false);
+							completeButton.setEnabled(false);
+							break;
+						case 1:
+							cancelButton.setEnabled(false);
+							editButton.setEnabled(false);
+							viewButton.setEnabled(true);
+							completeButton.setEnabled(false);
+							break;
+						case 2:
+							cancelButton.setEnabled(false);
+							editButton.setEnabled(false);
+							viewButton.setEnabled(true);
+							completeButton.setEnabled(true);
+							break;
+					}
+				}
+			}
+		});
+		statusBox.setBounds(10, 111, 88, 21);
+		ticket.add(statusBox);
+		
+		JPanel defaultPanel = new JPanel();
+		ticketPanel.setLayer(defaultPanel, 0);
+		defaultPanel.setBounds(0, 0, 222, 258);
+		ticketPanel.add(defaultPanel);
+		defaultPanel.setLayout(null);
+		
+		JButton createButton = new JButton("New Ticket");
+		createButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == createButton) {
+					defaultPanel.setVisible(false);
+					ticket.setVisible(true);
+					statusBox.setSelectedIndex(0);
+				}
+			}
+		});
+		createButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		createButton.setBounds(60, 103, 100, 50);
+		defaultPanel.add(createButton);
 	}
 }
